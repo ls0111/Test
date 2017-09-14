cd8267e69ea2315cd0db1ddf708e157a8532fe2d
lys <549014291@qq.com>
2017-09-14 09:35:13 +0800

    supplement some config files

---
+++ chapter2/src/main/java/com/smart/web/LoginCommand.java
@@ -0,0 +1,22 @@
+package com.smart.web;
+/**
+* @author 作者 louys:
+* @version 创建时间：2017年9月13日 上午9:05:31
+* 类说明
+*/
+public class LoginCommand {
+	String userName;
+	String password;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
