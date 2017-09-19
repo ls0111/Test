747c591d805938a793f5a83baa3379e02a43b890
lys <549014291@qq.com>
2017-09-19 15:57:22 +0800

    accumplished chapter3

---
+++ chapter3/src/main/java/com/smart/dao/LoginLogDao.java
@@ -0,0 +1,34 @@
+package com.smart.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.smart.domain.LoginLog;
+
+@Repository
+public class LoginLogDao {
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public int getMatchCount(String userName, String password) {
+		String sql = "select count(1) from t_user where user_name = ? and password = ?";
+		List list = jdbcTemplate.queryForList(sql, userName, password);
+		if (null == list) {
+			return 0;
+		}
+		return list.size();
+	}
+	
+	public void insertLoginLog(LoginLog loginLog){
+		Object[] args = new Object[]{loginLog.getUserId(),loginLog.getIp(),loginLog.getLoginDate()};
+		jdbcTemplate.update("insert into t_login_log(user_id,ip,login_datetime) values (?,?,?)",args);
+	}
+}
---
+++ chapter3/src/main/java/com/smart/web/LoginController.java
@@ -0,0 +1,43 @@
+package com.smart.web;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.smart.domain.User;
+import com.smart.service.UserService;
+
+/**
+ * @author 作者 louys:
+ * @version 创建时间：2017年9月13日 上午8:51:45 类说明
+ */
+@Controller
+public class LoginController {
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping(value = {"/","/index.html"})
+	public ModelAndView loginPage() {
+		return new ModelAndView("login");
+	}
+
+	@RequestMapping(value = "/loginCheck.html")
+	public ModelAndView loginCheck(HttpServletRequest req, LoginCommand loginCommand) {
+		boolean isValidUser = userService.hasMatchUser(loginCommand.getUserName(), loginCommand.getPassword());
+		if (!isValidUser) {
+			return new ModelAndView("login", "error", "用户名或者密码错误！");
+		} else {
+			User user = userService.findUserByUserName(loginCommand.getUserName());
+			user.setLastIp(req.getLocalAddr());
+			user.setLastVisit(new Date());
+			userService.loginSuccess(user);
+			req.getSession().setAttribute("user", user);
+		}
+		return new ModelAndView("main");
+	}
+}
